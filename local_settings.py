ec95d0b638f8fa1d3dedb573dda56a3847fbfd8c
wanlingt <wanling210@gmail.com>
2021-06-06 17:10:07 +0800

    added local_settings.py

---
+++ local_settings.py
@@ -0,0 +1,9 @@
+from os import environ
+
+ACCESS_KEY = environ["ACCESS KEY"]
+ACCESS_SECRET = environ["ACCESS_SECRET"]
+CONSUMER_KEY = environ["CONSUMER_KEY"]
+CONSUMER_SECRET = environ["CONSUMER_SECRET"]
+
+if __name__ == "__main__":
+    pass
