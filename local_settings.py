e95a8b938dcb861435590caebe70a0ab03da3c68
wanlingt <wanling210@gmail.com>
2021-06-06 17:11:52 +0800

    fixed typo

--- local_settings.py
+++ local_settings.py
@@ -1,6 +1,6 @@
 from os import environ
 
-ACCESS_KEY = environ["ACCESS KEY"]
+ACCESS_KEY = environ["ACCESS_KEY"]
 ACCESS_SECRET = environ["ACCESS_SECRET"]
 CONSUMER_KEY = environ["CONSUMER_KEY"]
 CONSUMER_SECRET = environ["CONSUMER_SECRET"]
